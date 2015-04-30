e7673a73fb978437864d0bfd7177742724fb447d
Chase Roberts <crobertsbmw@gmail.com>
2015-04-30 14:26:35 -0600

    merging formatting

--- deck/views.py
+++ deck/views.py
@@ -5,14 +5,14 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate, login, logout
 from deck.models import User, Deck, card_to_dict
 
-def _getRequestVar(request, key):
+def _get_request_var(request, key):
     if request.method == 'POST':
         return request.POST.get(key, 1)
     else:
         return request.GET.get(key, 1)
 
 def shuffle(request, key=''):
-    deck_count = int(_getRequestVar(request, 'deck_count'))
+    deck_count = int(_get_request_var(request, 'deck_count'))
     if deck_count > 20:
         return HttpResponse(json.dumps({'success':False,'error':'The max number of Decks is 20.'}), content_type="application/json")
     if key:
@@ -25,7 +25,7 @@ def shuffle(request, key=''):
     return HttpResponse(json.dumps(resp), content_type="application/json")
 
 def draw(request, key):
-    card_count = int(_getRequestVar(request, 'count'))
+    card_count = int(_get_request_var(request, 'count'))
     deck = Deck.objects.get(key=key)
     cards = deck.stack[0:card_count]
     deck.stack = deck.stack[card_count:]
