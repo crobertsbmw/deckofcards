ef37cbc61cf1edf412e8c34a1076a831f1c00f36
Chase Roberts <crobertsbmw@gmail.com>
2016-04-03 09:40:39 -0600

    adding clean command

---
+++ deck/management/commands/scrape.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+import datetime
+from deck.models import Deck
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        two_weeks_ago = datetime.datetime.now() - datetime.timedelta(days=100)
+        decks = Deck.objects.filter(last_used__lt=two_weeks_ago)
+        num = decks.count()
+        decks.delete()
+        print(str(num) + " decks deleted from db.")
