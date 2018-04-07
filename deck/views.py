64024709dbaa12c1cad34addeb646c2b8212b28b
Yakup Adakli <yakup.adakli@gmail.com>
2018-04-07 17:17:24 +0300

    Duplicated CARDS array removed.

--- deck/views.py
+++ deck/views.py
@@ -3,12 +3,7 @@ import random
 
 from django.shortcuts import HttpResponse
 
-from deck.models import Deck, card_to_dict
-
-CARDS = ['AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '0S', 'JS', 'QS', 'KS',
-         'AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '0D', 'JD', 'QD', 'KD',
-         'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '0C', 'JC', 'QC', 'KC',
-         'AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '0H', 'JH', 'QH', 'KH']
+from deck.models import Deck, card_to_dict, CARDS
 
 
 def _get_request_var(request, key, default=1):
