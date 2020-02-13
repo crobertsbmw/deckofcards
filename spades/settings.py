4200ffd5eb2c6f249e8592ddbb1b010b1d653c6e
Chase Roberts <crobertsbmw@gmail.com>
2020-02-13 16:14:53 -0700

    Upgrading to django 2.2

--- spades/settings.py
+++ spades/settings.py
@@ -50,13 +50,12 @@ INSTALLED_APPS = (
     'corsheaders',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
