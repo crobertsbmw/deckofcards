fa1179d309bfc10cc6ae791afa30632ce4b994d4
Chase Roberts <chase@mantisx.com>
2025-03-27 12:08:27 -0600

    Updating button link for CE for Babies

--- deck/management/commands/clean.py
+++ deck/management/commands/clean.py
@@ -7,8 +7,18 @@ from deck.models import Deck
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        two_weeks_ago = datetime.datetime.now() - datetime.timedelta(days=20)
+        two_weeks_ago = datetime.datetime.now() - datetime.timedelta(days=14)
         decks = Deck.objects.filter(last_used__lt=two_weeks_ago)
         num = decks.count()
         decks.delete()
         print(str(num) + " decks deleted from db.")
+
+from django.db import connection
+
+with connection.cursor() as cursor:
+    cursor.execute("DELETE reltuples::bigint FROM pg_class WHERE relname = 'deck_deck';")
+    row = cursor.fetchone()
+    print("Approximate count:", row[0])
+
+
+
